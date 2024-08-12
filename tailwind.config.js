d44d89ff2d06ac2c127cc8b5e8bdd01f6946b1c5
HUMILITY001 <humilityobarue@Gmail.com>
2024-08-11 23:49:31 -1100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/main.html"],
+  theme: {
+    extend: {
+      colors: {
+        '': '#201E43',
+        '': '#134B70',
+        '': '#508C9B',
+        '': '#EEEEEE',
+        '': '#ff3131',
+        '': '8px',
+
+      },
+    },
+  },
+  plugins: [],
+}
+
